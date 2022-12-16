b7c527f6abe4e1e3c8d672cf56cfae26a1c2e97e
bruna.sousa <brunamavigna2@gmail.com>
2022-12-16 10:01:01 -0300

    estudo sobre select

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl} from '@angular/forms'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  {
   title = 'select';
+
+  toppings = new FormControl('');
+
+  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
+  
+ constructor(){}
+
+  // changes(event: any){
+  //   console.log(event)
+  // }
+  ngOnInit():void{
+    this.toppings = new FormControl({
+      
+    })
+    
+  //   const newLocal = this.toppings.get('select').valueChanges.subscribe((x: any)  => {
+  //   console.log(x); 
+  // })
+
+  this.toppings.valueChanges.subscribe((value) => {
+    console.log(value ,'jfkf');
+  })
+
+  }
+
+  
+
+
+  changes2(event: any){
+    console.log(event)
+    
+    
+    
+  }
+
+   
 }
